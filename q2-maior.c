0c27a1cd20851fac439b01028d1a0c581181af73
Letícia Araujo <162067642+leletisu@users.noreply.github.com>
2025-08-24 21:46:50 -0300

    Add files via upload

---
+++ q2-maior.c
@@ -0,0 +1,29 @@
+
+
+
+#include <stdio.h>
+
+
+int main(){
+   int n1;
+   int n2;
+  
+   printf("Insira um número: ");
+   scanf("%d", &n1);
+
+
+   printf("Insira outro número: ");
+   scanf("%d", &n2);
+
+
+   if (n1 > n2){
+       printf("Maior número: %d\n", n1);
+   }
+
+
+   else{
+       printf("Maior número: %d\n", n2);
+   }
+  
+   return 0;
+}
